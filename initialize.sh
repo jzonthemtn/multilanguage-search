8beb294a11457dcf83067539f6d9e7f781ff24c7
mtnfog <jeffrey@mtnfog.com>
2018-03-21 18:57:15 -0400

    Building both jars.

--- initialize.sh
+++ initialize.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+mkdir -p files
+
 # Download the Apache Joshua en-de archive.
-wget https://mtnfog-public.s3.amazonaws.com/haystack-04-2018/apache-joshua-en-de-2017-01-31-phrase.tgz
+wget https://www.dropbox.com/sh/3uag3az9imyih0x/AADC0g3jnBiVM2KmoJCeFsapa/apache-joshua-en-de-2017-01-31-phrase.tgz
 gunzip -c apache-joshua-en-de-2017-01-31-phrase.tgz | tar xvf -
-mv apache-joshua-en-de-2017-01-31 ./files/
+mv apache-joshua-en-de-2017-01-31 ./files/apache-joshua-en-de-2017-01-31
 rm apache-joshua-en-de-2017-01-31-phrase.tgz
 
 # Download the index of a Wikipedia subset of German articles.
@@ -13,6 +15,6 @@ mv index ./files/
 rm index.zip
 
 # Download the OpenNLP language detection model.
-wget http://www.gtlib.gatech.edu/pub/apache/opennlp/models/langdetect/1.8.3/langdetect-183.bin -O ./files/
+wget http://www.gtlib.gatech.edu/pub/apache/opennlp/models/langdetect/1.8.3/langdetect-183.bin -O ./files/langdetect-183.bin
 
 
--- run-wikipediaimporter.sh
+++ run-wikipediaimporter.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 DIR=`pwd`
 export MLS_PATH="$DIR/files/"
-java -Xmx8g -jar ./target/mlsrunner.jar
-
+java -jar ./target/wikipediaimport.jar
